a6a879872d8c9a0ce372a601474b6910ca39a81c
CourseraGit70 <Greg.Tyler@smsciences.com>
2014-05-19 16:41:57 -0700

    Completed Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix creates a matrix that can cache it's computed inverse.
+# cacheSolve actually does the computation and stores it in the cache 
 
-## Write a short comment describing this function
+# creates the matrix, creates an empty cache, provides setters and getters for working with the values
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrixVal = matrix()) { #initialize a blank matrix if one isn't provided
+  cache <- NULL                                     #initialize blank cache
+  set <- function(y) {                              #create a setter method called set
+    matrixVal <<- y                                 #put the value into the matrix variable
+    cache <<- NULL                                  #empty the cache since the matrix just changed
+  }
+  get <- function() matrixVal                       #create a getter method called get that retuns the matrix value in x
+  setcache <- function(cachein) cache <<- cachein   #create a setter method for setting the cache in the parent scope
+  getcache <- function() cache                      #returns the value in the cache
+  list(set = set, get = get,                        #defines a list of names for easy access
+       setcache = setcache,
+       getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## loads the cache, returns it if it's unchanged and not empty, otherwise calculates the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrixVal, ...) {   #loads the matrix and anything else neccessary
+  cache <- matrixVal$getcache()            #loads the cached matrix into the local cache
+  if(!is.null(cache)) {                    #checks if something got loaded
+    message("getting cached data")         #informs the user
+    return(cache)                          #returns the cache value
+  }
+  data <- matrixVal$get()                  #gets the matrix for computation
+  cache <- solve(data)                     #calculates the inverse and puts it in the local cache
+  matrixVal$setcache(cache)                #loads the local cache value into the parent cache
+  cache                                    #returns the cached value
 }
